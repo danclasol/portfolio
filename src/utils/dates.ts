180ffbcb6a147088a6c3df07bc48f643db7102c9
Daniel <danclasol16@gmail.com>
2024-02-06 17:01:41 +0100

    Add feature i18n (es,en)

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,7 +1,17 @@
+import { getI18N } from "../i18n";
+
 export function getDateDifference(
   date_start: string,
-  date_end?: string
+  date_end?: string,
+  currentLocale: string = "es"
 ): string {
+  const i18n = getI18N({ currentLocale });
+  const yearLabel = i18n.YEAR_LABEL;
+  const yearsLabel = i18n.YEARS_LABEL;
+  const monthLabel = i18n.MONTH_LABEL;
+  const monthsLabel = i18n.MONTHS_LABEL;
+  const dateLinkLabel = i18n.DATES_LINK_LABEL;
+
   // Parse the input strings into Date objects
   const start = new Date(date_start);
   const end = new Date(date_end || new Date());
@@ -18,10 +28,14 @@ export function getDateDifference(
 
   monthsDiff++;
 
-  const formattedDifYear = `(${yearsDiff} year${yearsDiff > 1 ? "s" : ""} `;
+  const formattedDifYear = `(${yearsDiff} ${
+    yearsDiff > 1 ? yearsLabel : yearLabel
+  } `;
   const formattedDifMonth =
     monthsDiff > 0
-      ? `and ${monthsDiff} month${monthsDiff > 1 ? "s" : ""})`
+      ? `${dateLinkLabel} ${monthsDiff} ${
+          monthsDiff > 1 ? monthsLabel : monthLabel
+        })`
       : `)`;
 
   return formattedDifYear + formattedDifMonth;
