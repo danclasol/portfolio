331b5f02baf6b9b1e23f7e606b882604bbf558f2
Daniel <danclasol16@gmail.com>
2024-02-12 13:38:39 +0100

    Add language selector

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,45 +1,59 @@
-export const LANGUAGES: Record<string, { code: string; name: string }> = {
+import { getI18n } from "@i18n/index";
+import SpainFlagIcon from "@components/icons/SpainFlagIcon.astro";
+import EnglishFlagIcon from "@components/icons/EnglishFlagIcon.astro";
+
+export const LANGUAGES: Record<
+  string,
+  { code: string; name: string; flag: typeof EnglishFlagIcon }
+> = {
   en: {
     code: "en",
-    name: "English",
+    name: "EN",
+    flag: EnglishFlagIcon,
   },
   es: {
     code: "es",
-    name: "Español",
+    name: "ES",
+    flag: SpainFlagIcon,
   },
 };
 
 export const defaultLang = "es";
 export const showDefaultLang = false;
 
+const i18nES = getI18n({ currentLocale: "es" });
+const i18nEN = getI18n({ currentLocale: "en" });
+
 export const ui = {
   es: {
-    "nav.home": "Inicio",
-    "nav.experiencia": "Experiencia",
-    "nav.projects": "Proyectos",
-    "nav.about-me": "Sobre mí",
-    "nav.contact": "Contacto",
+    "nav.home": `${i18nES.HEADER_HOME}`,
+    "nav.experience": `${i18nES.HEADER_EXPERIENCE}`,
+    "nav.projects": `${i18nES.HEADER_PROJECTS}`,
+    "nav.about-me": `${i18nES.HEADER_ABOUT_ME}`,
+    "nav.contact": `${i18nES.HEADER_CONTACT}`,
   },
   en: {
-    "nav.home": "Home",
-    "nav.experiencia": "Experience",
-    "nav.projects": "Projects",
-    "nav.about-me": "About me",
-    "nav.contact": "Contact",
+    "nav.home": `${i18nEN.HEADER_HOME}`,
+    "nav.experience": `${i18nEN.HEADER_EXPERIENCE}`,
+    "nav.projects": `${i18nEN.HEADER_PROJECTS}`,
+    "nav.about-me": `${i18nEN.HEADER_ABOUT_ME}`,
+    "nav.contact": `${i18nEN.HEADER_CONTACT}`,
   },
 } as const;
 
 export const routes = {
   es: {
-    experience: "experiencia",
-    projects: "proyectos",
-    "about-me": "sobre-mi",
-    contact: "contacto",
+    home: `/#${i18nES.ROUTE_HOME}`,
+    experience: `/#${i18nES.ROUTE_EXPERIENCE}`,
+    projects: `/#${i18nES.ROUTE_PROJECTS}`,
+    "about-me": `/#${i18nES.ROUTE_ABOUT_ME}`,
+    contact: `/#${i18nES.ROUTE_CONTACT}`,
   },
   en: {
-    experience: "experience",
-    projects: "projects",
-    "about-me": "about-me",
-    contact: "contact",
+    home: `/en/#${i18nEN.ROUTE_HOME}`,
+    experience: `/en/#${i18nEN.ROUTE_EXPERIENCE}`,
+    projects: `/en/#${i18nEN.ROUTE_PROJECTS}`,
+    "about-me": `/en/#${i18nEN.ROUTE_ABOUT_ME}`,
+    contact: `/en/#${i18nEN.ROUTE_CONTACT}`,
   },
 };
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang, showDefaultLang, routes } from "./ui";
+import { ui, routes, defaultLang, showDefaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split("/");
@@ -12,54 +12,8 @@ export function useTranslations(lang: keyof typeof ui) {
   };
 }
 
-export function useTranslatedPath(lang: keyof typeof ui) {
-  return function translatePath(path: string, l: string = lang) {
-    const pathName = path.replaceAll("/", "");
-    const hasTranslation =
-      defaultLang !== l &&
-      (routes[l as keyof typeof routes] as Record<string, string>)[pathName] !==
-        undefined;
-    const translatedPath = hasTranslation
-      ? "/" +
-        (routes[l as keyof typeof routes] as Record<string, string>)[pathName]
-      : path;
-
-    return !showDefaultLang && l === defaultLang
-      ? translatedPath
-      : `/${l}${translatedPath}`;
+export function useTranslationsRoutes(lang: keyof typeof routes) {
+  return function r(key: keyof (typeof routes)[typeof defaultLang]) {
+    return routes[lang][key] || routes[defaultLang][key];
   };
-}
-
-export function getRouteFromUrl(url: URL): string | undefined {
-  const pathname = new URL(url).pathname;
-  const parts = pathname?.split("/");
-  const path = parts.pop() || parts.pop();
-
-  if (path === undefined) {
-    return undefined;
-  }
-
-  const currentLang = getLangFromUrl(url);
-
-  if (defaultLang === currentLang) {
-    const route = Object.values(routes)[0];
-    return route[path as keyof typeof route] !== undefined
-      ? route[path as keyof typeof route]
-      : undefined;
-  }
-
-  const getKeyByValue = (
-    obj: Record<string, string>,
-    value: string
-  ): string | undefined => {
-    return Object.keys(obj).find((key) => obj[key] === value);
-  };
-
-  const reversedKey = getKeyByValue(routes[currentLang], path);
-
-  if (reversedKey !== undefined) {
-    return reversedKey;
-  }
-
-  return undefined;
 }
