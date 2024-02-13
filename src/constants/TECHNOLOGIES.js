12b3ce1c4d7b5500b6aad0663c99820bcf148460
Daniel <danclasol16@gmail.com>
2024-02-13 14:12:03 +0100

    Update Light Mode Styles

--- src/constants/TECHNOLOGIES.js
+++ src/constants/TECHNOLOGIES.js
@@ -8,32 +8,32 @@ import TypeScriptIcon from "@components/icons/TypeScriptIcon.astro";
 export const TECHNOLOGIES = {
   REACT: {
     name: "React",
-    class: "bg-[#23262e] text-[#05d7fd] border-[#05d7fd]",
+    class: "dark:bg-[#23262e] text-[#05d7fd] border-[#05d7fd]",
     icon: ReactIcon,
   },
   TAILWIND: {
     name: "Tailwind CSS",
-    class: "bg-[#23262e] text-[#39bcf8] border-[#39bcf8]",
+    class: "dark:bg-[#23262e] text-[#39bcf8] border-[#39bcf8]",
     icon: TailwindIcon,
   },
   ELECTRON: {
     name: "Electron",
-    class: "bg-[#23262e] text-[#05d7fd] border-[#05d7fd]",
+    class: "dark:bg-[#23262e] text-[#05d7fd] border-[#05d7fd]",
     icon: ElectronIcon,
   },
   TYPESCRIPT: {
     name: "TypeScript",
-    class: "bg-[#23262e] text-[#3078c7] border-[#3078c7]",
+    class: "dark:bg-[#23262e] text-[#3078c7] border-[#3078c7]",
     icon: TypeScriptIcon,
   },
   JAVA: {
     name: "Java",
-    class: "bg-[#23262e] text-[#e66e01] border-[#e66e01]",
+    class: "dark:bg-[#23262e] text-[#e66e01] border-[#e66e01]",
     icon: JavaIcon,
   },
   ORACLE: {
     name: "PL/SQL",
-    class: "bg-[#23262e] text-[#c84735] border-[#c84735]",
+    class: "dark:bg-[#23262e] text-[#c84735] border-[#c84735]",
     icon: OracleIcon,
   },
 };
